84a5517c9c3c8871f07ac551e125898c6a1e6392
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-05-08 20:25:58 +0300

    commit lesson

--- src/main/java/services/BrowsersService.java
+++ src/main/java/services/BrowsersService.java
@@ -6,48 +6,50 @@ import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
-import java.util.Locale;
 
 public class BrowsersService {
     private WebDriver driver = null;
-    private DriverManagerType driverManagerType = null;
 
     public BrowsersService() {
-        switch (ReadProperties.browserName().toLowerCase(Locale.ROOT)) {
+        switch (ReadProperties.browserName().toLowerCase()) {
             case "chrome":
-                driverManagerType = DriverManagerType.CHROME;
-                WebDriverManager.getInstance(driverManagerType).setup(); // настройка браузера на локальном компьютере
-                driver = new ChromeDriver(getChromeOptions());
-                break;
+                DriverManagerType driverManagerType = DriverManagerType.CHROME;
+                WebDriverManager.getInstance(driverManagerType).setup();
 
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setHeadless(ReadProperties.isHeadless());
+                chromeOptions.addArguments("--disable-gpu");
+                //chromeOptions.addArguments("--window-size=1920,1200");
+                chromeOptions.addArguments("--ignore-certificate-errors");
+                chromeOptions.addArguments("--silent");
+                chromeOptions.addArguments("--start-maximized");
+
+                driver = new ChromeDriver(chromeOptions);
+
+                break;
             case "firefox":
-                break;
+                driverManagerType = DriverManagerType.FIREFOX;
+                WebDriverManager.getInstance(driverManagerType).setup();
 
+                driver = new FirefoxDriver();
+                break;
             default:
                 System.out.println("Browser " + ReadProperties.browserName() + " is not supported.");
+                break;
         }
     }
 
     public WebDriver getDriver() {
-        driver.manage().window().maximize(); //отобразить на весь экран
-        driver.manage().deleteAllCookies(); // удалить все куки
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
-        return this.driver;
-    }
+        //driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        //driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(60));
 
-    public ChromeOptions getChromeOptions() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setHeadless(ReadProperties.isHeadless()); // запуск браузера без визуализации
-        chromeOptions.addArguments("--disable-gpu"); //отключение графического процессора
-        //chromeOptions.addArguments("--window-size=1920,1200"); // задаетс расширение экрана
-        chromeOptions.addArguments("--ignore-certificate-errors"); // игнорирует ошибки сертификата
-        chromeOptions.addArguments("--silent"); // браузер не будет задаать лишних вопросов
-        chromeOptions.addArguments("--start-maximized"); // развернет браузер на весь экран
-        // chromeOptions.addArguments("--incognito"); // запуск  браузера инкогнито
-
-        return chromeOptions;
+        return driver;
     }
 }
