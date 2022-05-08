0f96a2a967bfc49073b1af52b4135c48e9a7f12a
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-05-08 20:18:37 +0300

    add hw

--- src/test/java/tests/MilestonesTest.java
+++ src/test/java/tests/MilestonesTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class MilestonesTest extends BaseTest {
 
     @Test
