7d159e6fdc52da24083d9809d82000992b56843e
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-05-25 21:15:42 +0300

    commit lesson

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,34 +2,46 @@ package tests;
 
 import baseEntities.BaseTest;
 import configuration.ReadProperties;
+import models.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
+
     @Test
     public void successLoginTest() {
+        User user = new User();
+        user.setEmail(ReadProperties.username());
+        user.setPsw(ReadProperties.password());
+
         Assert.assertTrue(
-                loginStep.successLogin(
-                                ReadProperties.username(),
-                                ReadProperties.password()
-                        )
+                loginStep.successLogin(user)
                         .isPageOpened()
         );
     }
 
     @Test
     public void incorrectEmailLoginTest() {
+        User user = new User();
+        user.setEmail("sdsd");
+        user.setPsw(ReadProperties.password());
+
         Assert.assertEquals(
-                loginStep.incorrectLogin("sdsd", ReadProperties.password()).getErrorTextElement().getText(),
+                loginStep.incorrectLogin(user).getErrorTextElement().getText(),
                 "Email/Login or Password is incorrect. Please try again.",
                 "Неверное сообщение об ошибке");
+        Assert.assertTrue(false);
     }
 
     @Test
     public void incorrectPswLoginTest() {
+        User user = new User();
+        user.setEmail(ReadProperties.username());
+        user.setPsw("123");
+
         Assert.assertEquals(
-                loginStep.incorrectLogin(ReadProperties.username(), "123").getErrorTextElement().getText(),
+                loginStep.incorrectLogin(user).getErrorTextElement().getText(),
                 "Email/Login or Password is incorrect. Please try again.",
                 "Неверное сообщение об ошибке");
     }
--- src/test/java/tests/MenuTest.java
+++ src/test/java/tests/MenuTest.java
@@ -2,16 +2,18 @@ package tests;
 
 import baseEntities.BaseTest;
 import configuration.ReadProperties;
+import models.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.TopMenuPage;
 
 public class MenuTest extends BaseTest {
+    User user =new  User();
 
     @Test
     public void validate_Dashboard_Menu_Test() {
-        DashboardPage dashboardPage = loginStep.successLogin(ReadProperties.username(), ReadProperties.password());
+        DashboardPage dashboardPage = loginStep.successLogin(user);
         TopMenuPage topMenuPage = dashboardPage.topMenuPage;
 
         Assert.assertTrue(topMenuPage.isPageOpened());
@@ -19,7 +21,7 @@ public class MenuTest extends BaseTest {
 
     @Test
     public void validate_Projects_SideMenu_Test() {
-        loginStep.successLogin(ReadProperties.username(), ReadProperties.password());
+        loginStep.successLogin(user);
         Assert.assertTrue(navigationStep.navigateToProjectsPage().sideMenuPage.isPageOpened());
     }
 }
