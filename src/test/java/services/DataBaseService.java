652016d70f2f8c3823414b7e2562e6c30b2afaf6
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-06-20 21:53:03 +0300

    commit lesson

---
+++ src/test/java/services/DataBaseService.java
@@ -0,0 +1,87 @@
+package services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+public class DataBaseService {
+    Logger logger = LoggerFactory.getLogger(DataBaseService.class);
+
+    static final String HOST = "localhost";
+    static final String PORT = "5432";
+    static final String USER = "postgres";
+    static final String PSW = "1";
+    static final String DATABASE_NAME = "postgres";
+
+    static final String JDBC = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DATABASE_NAME; // создание строки соединения
+
+    Connection connection = null;
+    Statement statement = null;
+
+    public DataBaseService() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            logger.info(e.toString());
+            return;
+        }
+
+        try {
+            connection = DriverManager.getConnection(JDBC, USER, PSW);
+        } catch (SQLException throwables) {
+            logger.info(throwables.toString());
+        }
+
+        if (connection != null) {
+            logger.info("You successfully connected to database.");
+        } else {
+            logger.info("Что-то пошло не так");
+        }
+    }
+
+    // закрытие соединения
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            logger.info(throwables.toString());
+        }
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+
+    public Statement getStatement() {
+        try {
+            if (statement == null) {
+                statement = getConnection().createStatement();
+            }
+        } catch (SQLException ex) {
+            logger.info("Не удалось создать Statement...");
+        }
+
+        return statement;
+    }
+
+    // не возвращает результат выполнения запроса (insert, update, delete)
+    public void executeSQL(String sql) {
+        try {
+            logger.info("Результат выполнения запроса:" + getStatement().execute(sql));
+        } catch (SQLException e) {
+            logger.info(e.getMessage());
+        }
+    }
+
+    // возвращает результат выполнения запроса (select)
+    public ResultSet executeQuery(String sql) {
+        try {
+            return getStatement().executeQuery(sql);
+        } catch (SQLException ex) {
+            logger.info(ex.getMessage());
+        }
+
+        return null;
+    }
+}
---
+++ src/test/java/tests/dbTest/BaseDBTest.java
@@ -0,0 +1,36 @@
+package tests.dbTest;
+
+import dbEntities.CustomersTable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import services.DataBaseService;
+
+public class BaseDBTest {
+
+    public DataBaseService dataBaseService;
+    Logger logger = LoggerFactory.getLogger(BaseDBTest.class);
+    CustomersTable customersTable;
+
+    public BaseDBTest() {
+    }
+
+    @BeforeTest
+    public void setupConnection() {
+        dataBaseService = new DataBaseService();
+
+        customersTable = new CustomersTable(dataBaseService);
+        customersTable.dropTable();
+        customersTable.createCustomersTable();
+
+        customersTable.addCustomer("Иван", "Иванов", "ivanov@test.com", 28);
+        customersTable.addCustomer("Петр", "Петров", "petrov@test.com", 38);
+        customersTable.addCustomer("Марина", "Стасевич", "marina@test.com", 23);
+    }
+
+    @AfterTest
+    public void closeConnection() {
+        dataBaseService.closeConnection();
+    }
+}
