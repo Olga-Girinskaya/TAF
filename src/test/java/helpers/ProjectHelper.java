e7832a9f29ce54a521410b19be302d92b2ce4e6e
Olga-Girinskaya <girinskaya.olga@gmail.com>
2022-06-21 01:47:20 +0300

    commit hw

--- src/test/java/helpers/ProjectHelper.java
+++ src/test/java/helpers/ProjectHelper.java
@@ -3,9 +3,12 @@ package helpers;
 import configuration.Endpoints;
 import io.restassured.response.Response;
 import models.Project;
+import models.ProjectType;
 import org.apache.http.HttpStatus;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -36,4 +39,28 @@ public class ProjectHelper {
 
         return response.getBody().jsonPath().getList("projects", Project.class);
     }
+
+    public void addProject() {
+        Project project = Project.builder()
+                .name("Project_HW")
+                .typeOfProject(ProjectType.SINGLE_SUITE_BASELINES)
+                .build();
+
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("name", project.getName());
+        jsonAsMap.put("suite_mode", project.getTypeOfProject());
+
+        Project newProject = given()
+                .body(jsonAsMap)
+                .when()
+                .post(Endpoints.ADD_PROJECT)
+                .then()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .as(Project.class);
+
+        System.out.println(newProject.toString());
+    }
+
 }
